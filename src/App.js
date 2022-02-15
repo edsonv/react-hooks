a904ca393be5f8f1576ded4da16d56bb8fc33f6e
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-15 17:32:21 -0500

    01-useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <Header />
     </div>
   );
 }
